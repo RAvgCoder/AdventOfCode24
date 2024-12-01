cc7dc6fa66854fb7f415da62989b401107957bf9
Osebhulimen Egbor <os614875@dal.ca>
2024-12-01 06:33:11 -0400

    Day1 part 1 & 2 done

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,4 @@
-
+use std::collections::HashMap;
 use crate::utils::day_setup::Utils;
 
 /// Runs the Advent of Code puzzles for [Current Day](https://adventofcode.com/2021/day/1).
@@ -10,17 +10,49 @@ use crate::utils::day_setup::Utils;
 ///   If the result of any part does not match the expected value.
 pub fn run() {
     // run_part(day_func_part_to_run, part_num, day_num)
-    Utils::run_part(part1, 1, 0, None);
-    Utils::run_part(part2, 2, 0, None);
+    Utils::run_part(part1, 1, 1, Some(3574690));
+    Utils::run_part(part2, 2, 1, Some(22565391));
 }
 
-fn part1(input: Vec<String>) -> u64 {
-    println!("Part 1: {:#?}", input);
-    0
+fn part1(input: Vec<String>) -> u32 {
+    let (mut list1, mut list2) = parse_input(input);
+
+    list1.sort_unstable();
+    list2.sort_unstable();
+
+    list1.iter().zip(list2).map(|(a, b)| b.abs_diff(*a)).sum()
 }
 
-fn part2(input: Vec<String>) -> u64 {
-    println!("Part 2 {:#?}", input);
-    0
+fn parse_input(input: Vec<String>) -> (Vec<u32>, Vec<u32>) {
+    input.iter().map(|x| {
+        let mut split = x.split_whitespace();
+        (split.next().unwrap().parse::<u32>().unwrap(), split.next().unwrap().parse::<u32>().unwrap())
+    }).fold((vec![], vec![]), |(mut list1, mut list2), (a, b)| {
+        list1.push(a);
+        list2.push(b);
+        (list1, list2)
+    })
+}
+
+fn part2(input: Vec<String>) -> u32 {
+    let (list1, list2) = parse_input(input);
+    
+    let mut tally_map = HashMap::with_capacity(list1.len());
+    
+    for e in list1 {
+        tally_map.entry(e).and_modify(|(tally, _)| {
+            *tally += 1;
+        }).or_insert((1, 0));
+    }
+    
+    for e in list2 {
+        tally_map.entry(e).and_modify(|(_, count)| {
+            *count += 1;
+        }).or_insert((0, 1));
+    }
+    
+    tally_map.iter().map(|(key, (tally, count))| {
+        (key * count) * tally
+    }).sum()
 }
                 
