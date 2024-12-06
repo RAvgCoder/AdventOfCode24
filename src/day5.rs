e75c9df17e4197188472d0cd311fcebc10cab564
Osebhulimen Egbor <os614875@dal.ca>
2024-12-06 17:39:38 -0400

    Completed day5 part1

--- src/day5.rs
+++ src/day5.rs
@@ -1,6 +1,7 @@
 use aoc_utils_rust::day_setup::Utils;
+use std::collections::{HashMap, HashSet};
 
-/// Runs the Advent of Code puzzles for [Current Day](https://adventofcode.com/202021/day/5).
+/// Runs the Advent of Code puzzles for [Current Day](https://adventofcode.com/2024/day/5).
 ///
 /// This function calls the `run_part` function from the `Utils` module to execute and time
 /// the solutions for both parts of the current day, checking them against the expected results.
@@ -9,16 +10,104 @@ use aoc_utils_rust::day_setup::Utils;
 ///   If the result of any part does not match the expected value.
 pub fn run() {
     // run_part(day_func_part_to_run, part_num, day_num)
-    Utils::run_part(part1, 1, 0, None);
-    Utils::run_part(part2, 2, 0, None);
+    Utils::run_part_single(part1, 1, 0, None);
+    Utils::run_part_single(part2, 2, 0, None);
 }
 
-fn part1(input: Vec<String>) -> u64 {
-    println!("Part 1: {:#?}", input);
+fn part1(book: Books) -> u32 {
+    book.partition().0.sum_middle_pages()
+}
+
+fn part2(book: Books) -> u32 {
+    // book.partition().1.sum_middle_pages()
     0
 }
 
-fn part2(input: Vec<String>) -> u64 {
-    println!("Part 2 {:#?}", input);
-    0
+#[derive(Debug)]
+struct Books {
+    rules: HashMap<u8, HashSet<u8>>,
+    books: Vec<Vec<u8>>,
+}
+
+struct UnorderedBooks {
+    rules: HashMap<u8, HashSet<u8>>,
+    unordered_pages: Vec<Vec<u8>>,
+}
+
+struct OrderedBooks {
+    ordered_pages: Vec<Vec<u8>>,
+}
+
+impl OrderedBooks {
+    fn sum_middle_pages(self) -> u32 {
+        self.ordered_pages
+            .into_iter()
+            .map(|page| page[page.len() / 2] as u32)
+            .sum()
+    }
+}
+
+impl Books {
+    fn partition(self) -> (OrderedBooks, UnorderedBooks) {
+        let mut ordered_pages = Vec::with_capacity(self.books.len());
+        let mut unordered_pages = Vec::with_capacity(self.books.len());
+
+        let mut page_set = HashSet::new();
+        'search_books: for book in self.books {
+            for page in &book {
+                if !self
+                    .rules
+                    .get(page)
+                    .map(|p| p.is_disjoint(&page_set))
+                    .unwrap_or(true)
+                {
+                    unordered_pages.push(book);
+                    page_set.clear();
+                    continue 'search_books;
+                }
+                page_set.insert(*page);
+            }
+            ordered_pages.push(book);
+            page_set.clear();
+        }
+
+        (
+            OrderedBooks { ordered_pages },
+            UnorderedBooks {
+                rules: self.rules,
+                unordered_pages,
+            },
+        )
+    }
+}
+
+impl From<Vec<String>> for Books {
+    fn from(value: Vec<String>) -> Self {
+        let mut rules_forward = HashMap::with_capacity(value.len());
+        let mut pages = Vec::with_capacity(value.len());
+
+        let mut value = value.into_iter();
+
+        while let Some(line) = value.next() {
+            if line.is_empty() {
+                break;
+            }
+
+            // 53|13
+            let (before, after) = line.split_at(2);
+            rules_forward
+                .entry(before.parse().unwrap())
+                .or_insert(HashSet::new())
+                .insert(after[1..].parse().unwrap());
+        }
+
+        for line in value {
+            pages.push(line.split(',').map(|x| x.parse().unwrap()).collect());
+        }
+
+        Self {
+            rules: rules_forward,
+            books: pages,
+        }
+    }
 }
