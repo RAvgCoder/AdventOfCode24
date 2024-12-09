b48e73af36305fba48470f0c2864e581a0b3fe58
Osebhulimen Egbor <os614875@dal.ca>
2024-12-08 20:39:51 -0400

    Finished day 8 p2

--- src/day8.rs
+++ src/day8.rs
@@ -12,16 +12,19 @@ use std::collections::{HashMap, HashSet};
 pub fn run() {
     // run_part(day_func_part_to_run, part_num, day_num)
     Utils::run_part(part1, 1, 8, Some(323));
-    Utils::run_part(part2, 2, 0, None);
+    Utils::run_part(part2, 2, 8, Some(1077));
 }
 
-fn part1(frequency_map: AntennaMap) -> u64 {
-    frequency_map.create_anti_node().len() as u64
+fn part1(frequency_map: AntennaMap) -> usize {
+    frequency_map
+        .create_anti_node(AntiNodeDistance::Twice)
+        .len()
 }
 
-fn part2(_input: Vec<String>) -> u64 {
-    // println!("Part 2 {:#?}", input);
-    0
+fn part2(frequency_map: AntennaMap) -> usize {
+    frequency_map
+        .create_anti_node(AntiNodeDistance::Unbounded)
+        .len()
 }
 
 #[derive(Debug)]
@@ -30,14 +33,20 @@ struct AntennaMap {
     grid_size: (u32, u32),
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum AntiNodeDistance {
+    Twice,
+    Unbounded,
+}
+
 impl AntennaMap {
-    fn create_anti_node(&self) -> HashSet<Coordinate> {
+    fn create_anti_node(&self, anti_node_distance: AntiNodeDistance) -> HashSet<Coordinate> {
         let mut anti_nodes = HashSet::new();
 
         for (_, coords) in self.antenna_map.iter() {
             for (i, c1) in coords.iter().enumerate() {
                 for c2 in coords[i + 1..].iter() {
-                    self.find_anti_node(*c1, *c2, &mut anti_nodes);
+                    self.find_anti_node(*c1, *c2, &mut anti_nodes, anti_node_distance);
                 }
             }
         }
@@ -45,7 +54,13 @@ impl AntennaMap {
         anti_nodes
     }
 
-    fn find_anti_node(&self, c1: Coordinate, c2: Coordinate, anti_node: &mut HashSet<Coordinate>) {
+    fn find_anti_node(
+        &self,
+        c1: Coordinate,
+        c2: Coordinate,
+        anti_node: &mut HashSet<Coordinate>,
+        anti_node_distance: AntiNodeDistance,
+    ) {
         let (dx, dy) = c1.slope_relative(c2);
         if dx == 0 {
             panic!("dx is 0 for c1: {:#?}, c2: {:#?}", c1, c2);
@@ -55,15 +70,36 @@ impl AntennaMap {
         let c1_temp = c1 - dx_dy_coord;
         let c2_temp = c2 + dx_dy_coord;
 
-        if self.is_valid(c1_temp) {
-            anti_node.insert(c1_temp);
-        }
-        if self.is_valid(c2_temp) {
-            anti_node.insert(c2_temp);
+        match anti_node_distance {
+            AntiNodeDistance::Twice => {
+                if self.in_bounds(c1_temp) {
+                    anti_node.insert(c1_temp);
+                }
+                if self.in_bounds(c2_temp) {
+                    anti_node.insert(c2_temp);
+                }
+            }
+            AntiNodeDistance::Unbounded => {
+                // Add the current coordinates to the anti_node set as they form part of the anti-node
+                anti_node.insert(c1);
+                anti_node.insert(c2);
+
+                // Add all the coordinates in the direction of the slope to the anti_node set
+                let mut c1_temp = c1_temp;
+                let mut c2_temp = c2_temp;
+                while self.in_bounds(c1_temp) {
+                    anti_node.insert(c1_temp);
+                    c1_temp -= dx_dy_coord;
+                }
+                while self.in_bounds(c2_temp) {
+                    anti_node.insert(c2_temp);
+                    c2_temp += dx_dy_coord;
+                }
+            }
         }
     }
 
-    fn is_valid(&self, coord: Coordinate) -> bool {
+    fn in_bounds(&self, coord: Coordinate) -> bool {
         let (i, j) = coord.into();
         (0..self.grid_size.0 as i32).contains(&i) && (0..self.grid_size.1 as i32).contains(&j)
     }
@@ -78,12 +114,18 @@ impl From<Vec<String>> for AntennaMap {
                     '.' | '#' => {}
                     other => {
                         let coord = Coordinate::new(i as i32, j as i32);
-                        antenna_map.entry(other).or_insert_with(Vec::new).push(coord);
+                        antenna_map
+                            .entry(other)
+                            .or_insert_with(Vec::new)
+                            .push(coord);
                     }
                 }
             }
         }
 
-        Self { antenna_map, grid_size: (input.len() as u32, input[0].len() as u32) }
+        Self {
+            antenna_map,
+            grid_size: (input.len() as u32, input[0].len() as u32),
+        }
     }
 }
