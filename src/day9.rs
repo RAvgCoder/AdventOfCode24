c49c928320dd1fe76e8e09c20a2b2bf5f78372a4
Osebhulimen Egbor <os614875@dal.ca>
2024-12-10 05:11:33 -0400

    Optimized day9 p2 by not allocating and operating on a disc but performing the calculations as I simulate movements

--- src/day9.rs
+++ src/day9.rs
@@ -28,26 +28,19 @@ struct DiskMap {
 
 impl DiskMap {
     fn un_fragmented_check_sum(self) -> u64 {
-        let mut disc = self.generate_disk();
         // (idx_in_disc, num_of_vals, nums)
-        let mut map_with_indices: Vec<(usize, (u8, u32))> = Vec::with_capacity(self.maps.len());
+        let mut map_num_with_indices: Vec<(usize, (u8, u32))> = Vec::with_capacity(self.maps.len()/2);
+        let mut map_spaces_with_indices: Vec<(usize, u8)> = Vec::with_capacity(self.maps.len()/2);
         let _ =
             self.maps
                 .iter()
                 .enumerate()
                 .fold((0, 0), |(acc_idx, gen_num), (map_idx, &count)| {
-                    map_with_indices.push((
-                        acc_idx,
-                        (
-                            count,
-                            if map_idx % 2 == 1 {
-                                // Skip numbers
-                                0
-                            } else {
-                                gen_num
-                            },
-                        ),
-                    ));
+                    if map_idx % 2 == 1 {
+                        map_spaces_with_indices.push((acc_idx, count));
+                    } else {
+                        map_num_with_indices.push((acc_idx, (count, gen_num)));
+                    }
                     (
                         acc_idx + count as usize,
                         if map_idx % 2 == 1 {
@@ -57,59 +50,39 @@ impl DiskMap {
                         },
                     )
                 });
+        
+        let mut map_num_ptr = map_num_with_indices.iter().rev();
 
-        let mut r_map_ptr = map_with_indices.len() - 1;
+        let mut check_sum = 0;
 
-        while r_map_ptr > 0 {
-            let (r_idx, (r_count, num)) = map_with_indices[r_map_ptr];
-            if let Some((l_map_idx, &(disc_idx, (_, _)))) = map_with_indices[..r_map_ptr]
+        while let Some(&(r_idx, (r_count, num))) = map_num_ptr.next() {
+            if let Some((l_map_idx, &(disc_idx, _))) = map_spaces_with_indices
                 .iter()
                 .enumerate()
-                .skip(1) // Start at odd index
-                .step_by(2) // Only loop through odd indices
-                .find(|&(_, &(_, (l_count, _)))| l_count >= r_count)
+                .find(|&(_, &(l_idx, l_count))| l_count >= r_count && l_idx < r_idx)
             // Find a space that can hold the values
             {
-                // Clear the moved values
-                for c in disc[r_idx..].iter_mut().take(r_count as usize) {
-                    *c = 0
+                // Add the moved value
+                for i in (disc_idx..).take(r_count as usize) {
+                    check_sum += num as u64 * i as u64;
                 }
 
-                // Add the moved value
-                for c in disc[disc_idx..].iter_mut().take(r_count as usize) {
-                    *c = num
-                }
-                let (l_idx, (l_count, _)) = &mut map_with_indices[l_map_idx];
+                let (l_idx, l_count) = &mut map_spaces_with_indices[l_map_idx];
                 *l_idx += r_count as usize;
                 *l_count -= r_count;
-            }
-            r_map_ptr -= 2;
-        }
-
-        disc.iter()
-            .enumerate()
-            .map(|(i, e)| if *e == 0 { 0 } else { i as u64 * *e as u64 })
-            .sum()
-    }
-
-    fn generate_disk(&self) -> Vec<u32> {
-        let mut disk = vec![0; self.maps.iter().map(|e| *e as usize).sum()];
-        let mut num = 0;
-        let mut idx = 0_usize;
-        for (i, &e) in self.maps.iter().enumerate() {
-            if i % 2 == 1 {
-                // Skips spaces
-                idx += e as usize
-            } else {
-                for _ in 0..e {
-                    disk[idx] = num;
-                    idx += 1
+                // remove it from consideration if it's empty for the next round
+                if l_count == &0 {
+                    map_spaces_with_indices.swap_remove(l_map_idx);
                 }
-                num += 1;
+            } else {
+                // If there are no spaces that can hold the values, then compute them
+                for i in (r_idx..).take(r_count as usize) {
+                    check_sum += num as u64 * i as u64;
+                }
             }
         }
 
-        disk
+        check_sum
     }
 
     fn fragmented_check_sum(mut self) -> usize {
@@ -121,7 +94,7 @@ impl DiskMap {
         let mut l_num = 0;
         let mut r_num = self.maps.len() / 2;
 
-        let mut count = 0;
+        let mut virtual_list_idx = 0;
 
         while l_ptr <= r_ptr {
             // Skip odd numbers coming from the left
@@ -149,15 +122,15 @@ impl DiskMap {
                 _ => {
                     if l_ptr % 2 == 1 {
                         // Add nums from the right to checksum
-                        check_sum += r_num * count;
+                        check_sum += r_num * virtual_list_idx;
                         self.maps[r_ptr] -= 1;
                         self.maps[l_ptr] -= 1;
-                        count += 1;
+                        virtual_list_idx += 1;
                     } else {
                         // Add nums from the left to checksum
-                        check_sum += l_num * count;
+                        check_sum += l_num * virtual_list_idx;
                         self.maps[l_ptr] -= 1;
-                        count += 1;
+                        virtual_list_idx += 1;
                     }
                 }
             }
