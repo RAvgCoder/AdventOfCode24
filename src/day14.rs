6a18a175534e6258a045e142535e00ca984ab2d1
Osebhulimen Egbor <os614875@dal.ca>
2024-12-14 14:40:38 -0400

    Day 14 p2 complete

--- src/day14.rs
+++ src/day14.rs
@@ -1,7 +1,7 @@
 use aoc_utils_rust::coordinate_system::Coordinate;
 use aoc_utils_rust::day_setup::Utils;
-use aoc_utils_rust::grid::unsized_grid::UnsizedGrid;
-use aoc_utils_rust::grid::GridMut;
+use aoc_utils_rust::grid::sized_grid::SizedGrid;
+use aoc_utils_rust::grid::Grid;
 use aoc_utils_rust::math::Math;
 use std::str::FromStr;
 
@@ -14,18 +14,28 @@ use std::str::FromStr;
 ///   If the result of any part does not match the expected value.
 pub fn run() {
     // run_part(day_func_part_to_run, part_num, day_num)
-    Utils::run_part(part1, 1, 14, None);
-    Utils::run_part(part2, 2, 0, None);
+    Utils::run_part(part1, 1, 14, Some(232253028));
+    Utils::run_part(part2, 2, 14, None);
 }
 
-fn part1(mut robot_simulation: RobotSimulation) -> u32 {
-    let map_size = (101, 103);
-    robot_simulation.determine_safety_factor(100, map_size)
+fn part1(mut robot_simulation: RobotSimulation<101, 103>) -> u32 {
+    const SIM_SECONDS: u32 = 100;
+    robot_simulation.bulk_simulate_robots(SIM_SECONDS);
+    robot_simulation.determine_safety_factor()
 }
 
-fn part2(input: Vec<String>) -> u64 {
-    println!("Part 2 {:#?}", input);
-    0
+type Buffer<'a> = (&'a mut SizedGrid<char, 103, 101>, &'a mut String);
+fn part2(mut robot_simulation: RobotSimulation<101, 103>) -> u16 {
+    let mut sized_grid = SizedGrid::new('_');
+    let mut buff = String::with_capacity(103 * 101);
+    for n in 1..u16::MAX {
+        robot_simulation.bulk_simulate_robots(1);
+        // Put the thread to sleep for a second
+        if robot_simulation.has_made_tree((&mut sized_grid, &mut buff)) {
+            return n;
+        }
+    }
+    panic!("No christmas trees found");
 }
 
 #[derive(Debug)]
@@ -47,42 +57,45 @@ impl Robot {
 }
 
 #[derive(Debug)]
-struct RobotSimulation {
+struct RobotSimulation<const WIDE: u32, const TALL: u32> {
     robot: Vec<Robot>,
 }
 
-impl RobotSimulation {
-    fn print_grid(&self, (wide, tall): (u32, u32)) {
-        let mut unsized_grid = UnsizedGrid::new(tall as _, wide as _, '.');
+impl<const WIDE: u32, const TALL: u32> RobotSimulation<WIDE, TALL> {
+    fn has_made_tree(&self, (grid, buff): Buffer) -> bool {
+        buff.clear();
+        // Clear the grid
+        for row in grid.iter_mut() {
+            row.for_each(|(_, e)| *e = '_');
+        }
+
         for robot in self.robot.iter() {
-            *unsized_grid.get_mut(&robot.pos.transpose()).unwrap() = '1';
+            *grid.get_mut(&robot.pos.transpose()).unwrap() = '#';
         }
-        for row in unsized_grid.iter_mut() {
-            for (c, e) in row {
-                if c.j == (wide as i32 / 2) {
-                    print!(" ");
-                } else {
-                    if c.i == ((tall as i32) / 2) {
-                        print!(" ");
-                        // print!("{}", e);
-                    } else {
-                        print!("{}", e);
-                    }
-                }
+
+        for row in grid.iter() {
+            for (_, e) in row {
+                buff.push(*e);
             }
-            println!()
+            buff.push('\n');
         }
+
+        buff.contains(&"#".repeat(10))
     }
 
-    fn determine_safety_factor(&mut self, seconds: u32, (wide, tall): (u32, u32)) -> u32 {
+    fn bulk_simulate_robots(&mut self, seconds: u32) {
         // Move each robot by the number of seconds disregarding out of bounds
         self.robot.iter_mut().for_each(|robot| {
             robot.move_robot(seconds);
             // Move the calibrate the robots to the correct position
-            robot.recalibrate(wide, tall);
+            robot.recalibrate(WIDE, TALL);
         });
+    }
 
-        // (wide, tall) => (11, 7)
+    fn determine_safety_factor(&self) -> u32 {
+        let (wide, tall) = (WIDE, TALL);
+        // Example (wide, tall) => (11, 7)
+        // Split into quadrants
         // Quadrants produced
         // ..... .....
         // ..1.. ..2..
@@ -91,8 +104,6 @@ impl RobotSimulation {
         // ..... .....
         // ..3.. ..4..
         // ..... .....
-
-        // Split into quadrants
         let q1 = (0..wide as i32 / 2, 0..tall as i32 / 2);
         let q2 = ((wide as i32 / 2) + 1..wide as i32, 0..tall as i32 / 2);
         let q3 = (0..wide as i32 / 2, (tall as i32 / 2) + 1..tall as i32);
@@ -104,8 +115,7 @@ impl RobotSimulation {
         [q1, q2, q3, q4]
             .into_iter()
             .map(|(quad_wide, quad_tall)| {
-                self
-                    .robot
+                self.robot
                     .iter()
                     .filter(|robot| {
                         quad_wide.contains(&robot.pos.i) && quad_tall.contains(&robot.pos.j)
@@ -116,7 +126,7 @@ impl RobotSimulation {
     }
 }
 
-impl From<Vec<String>> for RobotSimulation {
+impl<const WIDE: u32, const TALL: u32> From<Vec<String>> for RobotSimulation<WIDE, TALL> {
     fn from(value: Vec<String>) -> Self {
         let mut robots = Vec::with_capacity(value.len());
 
